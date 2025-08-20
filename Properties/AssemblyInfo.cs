6d718196f80f4e889dcdac377a6ddb84e9a2966a
GALIH RIDHO UTOMO <92840324+galihru@users.noreply.github.com>
2025-08-20 18:24:13 +0700

    Fix publisher metadata - now shows 'GALIH RIDHO UTOMO' in UAC dialogs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+// General Information about an assembly
+[assembly: AssemblyTitle("Dungeon Game")]
+[assembly: AssemblyDescription("Strategic AI-powered dungeon exploration game with Q-Learning algorithms")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GALIH RIDHO UTOMO")]
+[assembly: AssemblyProduct("Dungeon Game")]
+[assembly: AssemblyCopyright("Copyright © 2025 GALIH RIDHO UTOMO. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("12345678-1234-1234-1234-123456789012")]
+
+// Version information
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+
+// WPF-specific
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
